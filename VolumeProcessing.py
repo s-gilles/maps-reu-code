42a294b9a5a1eb2008099ea183be4542b928cb25
Peter Huston <peter.huston@otterbein.edu>
2014-07-11 00:27:41 -0400

    Started complicating manifold name niceness.

--- VolumeProcessing.py
+++ VolumeProcessing.py
@@ -166,9 +166,36 @@ class dataset:
         nms.extend(self.get_pared_manifolds(poly,root,vol))
         opt = ('', sys.float_info.max)
         for nm in nms:
-            if len(nm) < opt[1]:
+            if len(nm) < opt[1]:    # TODO: finish _niceness and replace len with it
                 opt = (nm,len(nm))
         return opt[0]
+
+def _niceness(nm):
+    n = 0.0
+    k = nm[0]
+    if k == 'm':
+        n += 0
+    elif k == 'v':
+        n += 1
+    elif k == 't':
+        n += 2
+    elif k == 'o':
+        n += 3
+    elif k == 'K':  # TODO: apply knot and link # penalties
+        n += 4
+    elif k == 'L':
+        n += 5
+    elif k == 'b':
+        n += 6
+    elif k == 'B':
+        n += 7
+    elif k == 'D':
+        n += 8
+    else:
+        n += 10
+    # n *= gap TODO
+    # apply dehn penalties TODO
+    return n
 
     # If some volumes differ by less than epsilon, combine them, keeping one of them arbitrarily.
     # Slightly nondeterministic; may not get large, dense (compared to epsilon) clumps if iteration order is unfavorable.

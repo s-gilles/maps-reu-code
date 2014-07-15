f875531daaf290a29aa31c27956f658d0fcb575e
Peter Huston <peter.huston@otterbein.edu>
2014-07-15 14:22:28 -0400

    Made getting psuedo-volumes more robust; added reading and writing VolumeData objects to and from csv files.

--- PseudoVols.py
+++ PseudoVols.py
@@ -7,20 +7,24 @@ from snappy import *
 def get_volume_data(man_nms, engine = 'magma', retrieve = True):
     recs = dict()
     for nm in man_nms:
-        var = Manifold(nm).ptolemy_variety(2,'all')
         try:
-            if retrieve:
-                sols = var.retrieve_solutions()
-            else:
-                raise Exception('Coding too lazy!') 
-        except:
-            sols = var.compute_solutions(engine = engine)
-        data = [(c.number_field(),c.volume_numerical()) for c in sols.flatten()]
-        for d in data:
-            for v in d[1]:
-                recs.setdefault(str(d[0]),list()).append((str(v),nm))
-        for k in recs.keys():   # remove duplicates
-            recs[k] = list(set(recs[k]))
+            var = Manifold(nm).ptolemy_variety(2,'all')
+            try:
+                if retrieve:
+                    sols = var.retrieve_solutions()
+                else:
+                    raise Exception('Coding too lazy!') 
+            except:
+                sols = var.compute_solutions(engine = engine)
+            data = [(c.number_field(),c.volume_numerical()) for c in sols.flatten()]
+            for d in data:
+                for v in d[1]:
+                    recs.setdefault(str(d[0]),list()).append((str(v),nm))
+            for k in recs.keys():   # remove duplicates
+                recs[k] = list(set(recs[k]))
+        except Exception as e:
+            print(str(e))+'; skipping '+nm
+            continue
     return VolumeData(data = recs)
 
 def get_potential_trace_fields(poly):
@@ -58,3 +62,38 @@ class VolumeData:
             new_data.setdefault(p,dict())
             new_data[p] = list(set(new_data[p].extend(other.get_volume_data(p))))
         return VolumeData(data = new_data)
+
+    # given an (open, ready to write) file object or valid filename, writes the data
+    def write_to_csv(self, output_file, seperator = ';', append = False):
+        try:
+            if type(output_file) == str:
+                f = open(output_file,'w')
+            else:
+                f = output_file
+            f.write('"TraceField"'+seperator+'"Volume"'+seperator+'"Manifold"'+'\n')
+            for p in self.get_polys():
+                for rec in self.get_volume_data(p):
+                    f.write('"'+p+'"'+seperator)
+                    f.write('"'+rec[0]+'"'+seperator)
+                    f.write('"'+rec[1]+'"\n')
+        finally:
+            if type(output_file) == str and f:
+                f.close()
+
+
+    # given an (open, ready to read data) file object or valid filename, reads the file and returns a VolumeData that would write it
+def read_volumedata_csv(infile, seperator = ';'):
+    try:
+        if type(infile) ==  str:
+            f = open(infile,'r')
+            f.readline()
+        else:
+            f = infile
+        data = dict()
+        for l in f.readlines():
+            w = l.strip('\n').replace('"','').split(seperator)
+            data.setdefault(w[0],list()).append(w[1:])
+        return VolumeData(data = data)
+    finally:
+        if type(infile) == str and f:
+            f.close()
